9071e582dd8c1f911bbc96125d1b3685ee820f32
pxk56518 <547284857@qq.com>
2022-08-30 13:20:48 +0800

    流水灯

--- GitStudy/FrmMain.Designer.cs
+++ GitStudy/FrmMain.Designer.cs
@@ -28,20 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.BtnTest = new System.Windows.Forms.Button();
+            this.BtnTest2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // BtnTest
+            // 
+            this.BtnTest.Location = new System.Drawing.Point(104, 356);
+            this.BtnTest.Name = "BtnTest";
+            this.BtnTest.Size = new System.Drawing.Size(75, 23);
+            this.BtnTest.TabIndex = 0;
+            this.BtnTest.Text = "开始";
+            this.BtnTest.UseVisualStyleBackColor = true;
+            this.BtnTest.Click += new System.EventHandler(this.BtnTest_Click);
+            // 
+            // BtnTest2
+            // 
+            this.BtnTest2.Location = new System.Drawing.Point(330, 356);
+            this.BtnTest2.Name = "BtnTest2";
+            this.BtnTest2.Size = new System.Drawing.Size(75, 23);
+            this.BtnTest2.TabIndex = 1;
+            this.BtnTest2.Text = "结束";
+            this.BtnTest2.UseVisualStyleBackColor = true;
+            this.BtnTest2.Click += new System.EventHandler(this.BtnTest2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(138, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(314, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(410, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "label5";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(590, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnTest2);
+            this.Controls.Add(this.BtnTest);
             this.Name = "FrmMain";
             this.Text = "GitStudy";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnTest;
+        private System.Windows.Forms.Button BtnTest2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- GitStudy/FrmMain.cs
+++ GitStudy/FrmMain.cs
@@ -16,5 +16,54 @@ namespace GitStudy
         {
             InitializeComponent();
         }
+
+        private void BtnTest_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+        /// <summary>
+        /// 通过控件名称获取到控件
+        /// </summary>
+        /// <param name="controlName">控件名称</param>
+        /// <param name="containChildControlWindow">包含子控件的窗体（比如窗体是[Controls]、panel是[panel.controls]）</param>
+        /// <returns></returns>
+        private Control GetControlOfName(string controlName, Control.ControlCollection containChildControlWindow)
+        {
+            if (string.IsNullOrEmpty(controlName)) return null;
+
+            foreach (Control item in containChildControlWindow)
+            {
+                if (item.Name.Equals(controlName))
+                {
+                    return item;
+                }
+            }
+
+            return null;
+        }
+        int GreenIndex = 0;
+        Control control = null;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0;i<5;i++) 
+            {
+                string controlName = "label" + (i+1);
+                control = GetControlOfName(controlName, this.Controls);
+                if ((GreenIndex % 5) == i && control != null)
+                {
+                    control.BackColor = Color.Red;
+                }
+                else 
+                {
+                    control.BackColor = Color.LightGray;
+                }
+            }
+            GreenIndex++;
+        }
+
+        private void BtnTest2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
     }
 }
