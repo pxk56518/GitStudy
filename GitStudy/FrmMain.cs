abfab93898d1d52ef25c0357c3739f088fac517d
pxk56518 <547284857@qq.com>
2022-08-30 15:01:09 +0800

    ttt

--- GitStudy/FrmMain.cs
+++ GitStudy/FrmMain.cs
@@ -68,7 +68,7 @@ namespace GitStudy
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label6.Text = "下一个序号将是:" + (GreenIndex+1).ToString();
+            label6.Text = "当前序号是:" + (GreenIndex).ToString();
         }
     }
 }
